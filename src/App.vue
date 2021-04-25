0837a21b2bdc9ed18d08819c18fcb5766eb9b173
robdrosenberg <robertdr85@gmail.com>
2021-04-24 19:50:20 -0700

    initial vite setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <header class="bg-white shadow" v-if="$route.meta.title">
+      <div
+        class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center"
+      >
+        <router-link to="/" class="mr-8 text-2xl hover:underline"
+          >Home</router-link
+        >
+        <router-link to="/favorites" class="hover:underline"
+          >Favorites</router-link
+        >
+      </div>
+    </header>
+
+    <router-view class="h-full" />
+  </main>
+</template>
---
+++ src/views/Favorites.vue
@@ -0,0 +1,15 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {},
+});
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>Favorite Episodes</h1>
+    </div>
+  </main>
+</template>
