55c1b6681790ba6c43a81a0e1ba0484d83c23a36
robdrosenberg <robertdr85@gmail.com>
2021-04-27 21:03:29 -0700

    remove about page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import Home from './views/Home.vue'
-import About from './views/About.vue'
 import Favorites from './views/Favorites.vue'
 import NotFound from './views/NotFound.vue'
 
@@ -7,14 +6,5 @@ import NotFound from './views/NotFound.vue'
 export const routes = [
   { path: '/', component: Home, meta: { title: 'Home' } },
   { path: '/favorites', component: Favorites, meta: { title: 'My Favorite Episodes' } },
-  {
-    path: '/about',
-    meta: { title: 'About' },
-    component: About,
-    // example of route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('./views/About.vue')
-  },
   { path: '/:path(.*)', component: NotFound },
 ]
